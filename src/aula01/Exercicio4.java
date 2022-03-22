b4b2c374237b91ec1366fb6880a9514ce92e3bcc
Jonathas Sodré <jonathas_sl@hotmail.com>
2022-03-22 19:04:26 -0300

    aula1

--- src/aula01/Exercicio4.java
+++ src/aula01/Exercicio4.java
@@ -8,7 +8,7 @@ import java.time.format.DateTimeFormatter;
 public class Exercicio4 {
     public static void main(String[] args) {
         String de= " de ";
-        System.out.println("Exercicio 1");
+        System.out.println("Exercicio 4");
         System.out.println("Formatar uma data no seguinte padrão: '19:00:00, 17 de março de 2022 (fuso: -03:00)'");
         var zonedDateTimeNow = ZonedDateTime.now();
         System.out.println(zonedDateTimeNow.format(DateTimeFormatter.ofPattern("HH:mm:ss, d MMMM y (xxxx)")));
